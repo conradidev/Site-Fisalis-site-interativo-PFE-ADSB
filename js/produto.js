7461209d6978267e605eb9e093799a184dd0fee0
conradidev <lauraconradip@gmail.com>
2025-04-16 10:48:04 -0300

    na paz do senhor

--- js/produto.js
+++ js/produto.js
@@ -5,7 +5,7 @@ const produtos = [
       id: 1,
       nome: "Conjunto Clássico Creme",
       preco: 130.99,
-      imagem: "/imagens/conjuntoCreme.jpg",
+      imagem: "imagens/conjuntoCreme.jpg",
       categoria: "roupa",
       colecao: "casual"
     },
@@ -13,7 +13,7 @@ const produtos = [
       id: 2,
       nome: "T-shirt Solta Verde",
       preco: 89.90,
-      imagem: "/imagens/tshirtVerde.jpg",
+      imagem: "imagens/tshirtVerde.jpg",
       categoria: "roupa",
       colecao: "casual"
     },
@@ -21,7 +21,7 @@ const produtos = [
       id: 3,
       nome: "Calça Branca Linho",
       preco: 160.90,
-      imagem: "/imagens/calcaLinho.jpg",
+      imagem: "imagens/calcaLinho.jpg",
       categoria: "roupa",
       colecao: "praia"
     },
@@ -29,7 +29,7 @@ const produtos = [
       id: 4,
       nome: "Macacão Elegante",
       preco: 180.90,
-      imagem: "/imagens/macacaElegante.jpg",
+      imagem: "imagens/macacaElegante.jpg",
       categoria: "roupa",
       colecao: "campo"
     },
@@ -37,7 +37,7 @@ const produtos = [
       id: 5,
       nome: "Calça Branca Algodão",
       preco: 150.99,
-      imagem: "/imagens/calcaAlgodao.jpg",
+      imagem: "imagens/calcaAlgodao.jpg",
       categoria: "roupa",
       colecao: "corporativo"
     },
@@ -45,7 +45,7 @@ const produtos = [
       id: 6,
       nome: "Vestido Moda Praia",
       preco: 110.99,
-      imagem: "/imagens/vestidoPraia.jpg",
+      imagem: "imagens/vestidoPraia.jpg",
       categoria: "roupa",
       colecao: "praia"
     },
@@ -53,7 +53,7 @@ const produtos = [
       id: 7,
       nome: "Vestido Princesa Campo",
       preco: 189.99,
-      imagem: "/imagens/vestidoCampo.jpg",
+      imagem: "imagens/vestidoCampo.jpg",
       categoria: "roupa",
       colecao: "campo"
     },
@@ -61,7 +61,7 @@ const produtos = [
       id: 8,
       nome: "Conjunto AllBlack",
       preco: 259.99,
-      imagem: "/imagens/roupa1.jpg",
+      imagem: "imagens/roupa1.jpg",
       categoria: "roupa",
       colecao: "corporativo"
     },
@@ -69,7 +69,7 @@ const produtos = [
       id: 9,
       nome: "Bolsa Vermelho Púrpura",
       preco: 40.99,
-      imagem: "/imagens/bolsa1.jpg",
+      imagem: "imagens/bolsa1.jpg",
       categoria: "bolsa",
       colecao: "corporativo"
     },
@@ -77,7 +77,7 @@ const produtos = [
       id: 10,
       nome: "Bolsa Branca Pura",
       preco: 50.99,
-      imagem: "/imagens/bolsa2.jpg",
+      imagem: "imagens/bolsa2.jpg",
       categoria: "bolsa",
       colecao: "campo"
     },
@@ -85,7 +85,7 @@ const produtos = [
       id: 11,
       nome: "Bolsa Versátil Marrom",
       preco: 80.99,
-      imagem: "/imagens/bolsa3.jpg",
+      imagem: "imagens/bolsa3.jpg",
       categoria: "bolsa",
       colecao: "corporativo"
     },
@@ -93,7 +93,7 @@ const produtos = [
       id: 12,
       nome: "Bolsa Charme",
       preco: 30.99,
-      imagem: "/imagens/bolsa4.jpg",
+      imagem: "imagens/bolsa4.jpg",
       categoria: "bolsa",
       colecao: "campo"
     },
@@ -101,7 +101,7 @@ const produtos = [
       id: 13,
       nome: "Bolsa Tricot",
       preco: 50.99,
-      imagem: "/imagens/bolsa5.jpg",
+      imagem: "imagens/bolsa5.jpg",
       categoria: "bolsa",
       colecao: "praia"
     },
@@ -109,7 +109,7 @@ const produtos = [
       id: 14,
       nome: "Bolsa Mar",
       preco: 69.99,
-      imagem: "/imagens/bolsa6.jpg",
+      imagem: "imagens/bolsa6.jpg",
       categoria: "bolsa",
       colecao: "praia"
     },
@@ -117,7 +117,7 @@ const produtos = [
       id: 15,
       nome: "Bolsa Prática Creme",
       preco: 49.99,
-      imagem: "/imagens/bolsa7.jpg",
+      imagem: "imagens/bolsa7.jpg",
       categoria: "bolsa",
       colecao: "casual"
     },
@@ -125,7 +125,7 @@ const produtos = [
       id: 16,
       nome: "Mochila Azul Bebe",
       preco: 39.99,
-      imagem: "/imagens/bolsa8.jpg",
+      imagem: "imagens/bolsa8.jpg",
       categoria: "bolsa",
       colecao: "casual"
     },
@@ -133,7 +133,7 @@ const produtos = [
       id: 17,
       nome: "Sandália Cinderela Branca",
       preco: 29.99,
-      imagem: "/imagens/sapato1.jpg",
+      imagem: "imagens/sapato1.jpg",
       categoria: "calçado",
       colecao: "praia"
     },
@@ -141,7 +141,7 @@ const produtos = [
       id: 18,
       nome: "Salto Médio Elegante",
       preco: 35.99,
-      imagem: "/imagens/sapato2.jpg",
+      imagem: "imagens/sapato2.jpg",
       categoria: "calçado",
       colecao: "praia"
     },
@@ -149,7 +149,7 @@ const produtos = [
       id: 19,
       nome: "Tênis Delicado Branco",
       preco: 49.99,
-      imagem: "/imagens/sapato3.jpg",
+      imagem: "imagens/sapato3.jpg",
       categoria: "calçado",
       colecao: "campo"
     },
@@ -157,7 +157,7 @@ const produtos = [
       id: 20,
       nome: "Tênis básico Creme",
       preco: 39.99,
-      imagem: "/imagens/sapato4.jpg",
+      imagem: "imagens/sapato4.jpg",
       categoria: "calçado",
       colecao: "campo"
     },
@@ -165,7 +165,7 @@ const produtos = [
       id:21,
       nome: "Tênis Conforto Dia a Dia",
       preco: 65.99,
-      imagem: "/imagens/sapato5.jpg",
+      imagem: "imagens/sapato5.jpg",
       categoria: "calçado",
       colecao: "casual"
     },
@@ -173,7 +173,7 @@ const produtos = [
       id: 22,
       nome: "Tênis Fem Delicado",
       preco: 65.99,
-      imagem: "/imagens/sapato6.jpg",
+      imagem: "imagens/sapato6.jpg",
       categoria: "calçado",
       colecao: "casual"
     },
@@ -181,7 +181,7 @@ const produtos = [
       id:23,
       nome: "Salto Bico Fino Branco",
       preco: 59.99,
-      imagem: "/imagens/sapato7.jpg",
+      imagem: "imagens/sapato7.jpg",
       categoria: "calçado",
       colecao: "corporativo"
     },
@@ -189,7 +189,7 @@ const produtos = [
       id: 24,
       nome: "Salto Bico Fino Branco Nude",
       preco: 59.99,
-      imagem: "/imagens/sapato8.jpg",
+      imagem: "imagens/sapato8.jpg",
       categoria: "calçado",
       colecao: "corporativo"
     },
@@ -197,7 +197,7 @@ const produtos = [
       id: 25,
       nome: "Brinco Flores Douradas",
       preco: 15.99,
-      imagem: "/imagens/brinco1.jpg",
+      imagem: "imagens/brinco1.jpg",
       categoria: "brinco",
       colecao: "corporativo"
     },
@@ -205,7 +205,7 @@ const produtos = [
       id: 26,
       nome: "Argola Dourada",
       preco: 14.99,
-      imagem: "/imagens/brinco2.jpg",
+      imagem: "imagens/brinco2.jpg",
       categoria: "brinco",
       colecao: "corporativo"
     },
@@ -213,7 +213,7 @@ const produtos = [
       id: 27,
       nome: "Brinco concha",
       preco: 10.99,
-      imagem: "/imagens/brinco3.jpg",
+      imagem: "imagens/brinco3.jpg",
       categoria: "brinco",
       colecao: "praia"
     },
@@ -221,7 +221,7 @@ const produtos = [
       id: 28,
       nome: "Brinco Argola Mar",
       preco: 16.99,
-      imagem: "/imagens/brinco4.jpg",
+      imagem: "imagens/brinco4.jpg",
       categoria: "brinco",
       colecao: "praia"
     },
@@ -229,7 +229,7 @@ const produtos = [
       id: 29,
       nome: "Brinco Floral",
       preco: 10.99,
-      imagem: "/imagens/brinco5.jpg",
+      imagem: "imagens/brinco5.jpg",
       categoria: "brinco",
       colecao: "campo"
     },
@@ -237,7 +237,7 @@ const produtos = [
       id: 30,
       nome: "Brinco Mini Flor",
       preco: 10.99,
-      imagem: "/imagens/brinco6.jpg",
+      imagem: "imagens/brinco6.jpg",
       categoria: "brinco",
       colecao: "campo"
     },
@@ -245,7 +245,7 @@ const produtos = [
       id: 31,
       nome: "Dois pares Argolas",
       preco: 20.99,
-      imagem: "/imagens/brinco7.jpg",
+      imagem: "imagens/brinco7.jpg",
       categoria: "brincos",
       colecao: "casual"
     },
@@ -253,7 +253,7 @@ const produtos = [
       id: 32,
       nome: "Brinco Triangular Dourado",
       preco: 20.99,
-      imagem: "/imagens/brinco8.jpg",
+      imagem: "imagens/brinco8.jpg",
       categoria: "brinco",
       colecao: "casual"
     }
